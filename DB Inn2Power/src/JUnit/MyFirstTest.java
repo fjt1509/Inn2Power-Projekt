095c0e2b2b3a9b432170ec0b74de51c6f14f3a92
Husso1997 <hussain1961@hotmail.com>
2020-12-29 20:40:24 +0100

    Junit and testng

---
+++ DB Inn2Power/src/JUnit/MyFirstTest.java
@@ -0,0 +1,15 @@
+package JUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyFirstTest {
+
+	@Test
+	void test() {
+		String k = "message";
+		assertTrue(true);
+	}
+
+}
---
+++ DB Inn2Power/src/db/inn2power/TestNG/MyFirstTest.java
@@ -0,0 +1,14 @@
+package db.inn2power.TestNG;
+
+import org.testng.annotations.Test;
+	import static org.testng.Assert.assertEquals;
+public class MyFirstTest {
+	
+	
+	
+  @Test
+  public void f() {
+	  String str = "TestNG is working fine";
+      assertEquals("TestNG is working fine", str);
+  }
+}
